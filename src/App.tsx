a7fb89b77576632fffab8e3ab9170a4925eb8512
kadoyau <el.psi.congroo@gmail.com>
2019-06-06 04:48:54 +0900

    pagesをコンポーネントに分割する

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import RootPage from './pages/Root';
+import WatchPage from './pages/Watch';
+import NoMatchPage from './pages/NoMatch';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={() => <h1>Root Page</h1>} />
-      <Route path="/watch/:id" component={() => <h1>Watch Page</h1>} />
-      <Route component={() => <h1>404</h1>} />
+      <Route exact path="/" component={RootPage} />
+      <Route path="/watch/:id" component={WatchPage} />
+      <Route component={NoMatchPage} />
     </Switch>
   </BrowserRouter>
 );
