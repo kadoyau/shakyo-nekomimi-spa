5375671aef71f1e9ac6044fa37ba3b739fc465c9
kadoyau <el.psi.congroo@gmail.com>
2019-06-05 13:52:56 +0900

    react-routerを使ってルーティングを描く

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Root Page</h1>} />
+      <Route path="/watch/:id" component={() => <h1>Watch Page</h1>} />
+      <Route component={() => <h1>404</h1>} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
