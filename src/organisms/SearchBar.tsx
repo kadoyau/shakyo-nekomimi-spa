76eaefdfdf07feeffe4753983dedf605b6ae6b30
kadoyau <el.psi.congroo@gmail.com>
2019-06-07 04:04:11 +0900

    useCallbackを利用して関数呼び出しを制限

--- src/organisms/SearchBar.tsx
+++ src/organisms/SearchBar.tsx
@@ -1,6 +1,6 @@
 import {AppBar, InputBase, Toolbar, withStyles} from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
-import React from "react";
+import React, {useCallback} from "react";
 import styles from './SearchBar.module.css';
 
 interface Props {
@@ -9,16 +9,20 @@ interface Props {
 
 const SearchBar = (props: Props) => {
     const {onChange} = props;
-    // コンポーネントがレンダリングされるたびに再定義される
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onChange(event.target.value);
-    };
+    // function componentで関数定義をする場合はuseCallbackを使うと、
+    // メモ化されるのでレンダリングのために再定義されるのを防げる
+    const handleChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            onChange(event.target.value);
+        },
+        []
+    );
     return (
         <AppBar position={"fixed"}>
             <Toolbar variant="dense">
                 <div className={styles.search}>
                     <SearchIcon fontSize="small"/>
-                    <StyleInputBase placeholder="search..." onChange={handleChange}/>
+                    <StyledInputBase placeholder="search..." onChange={handleChange}/>
                 </div>
             </Toolbar>
         </AppBar>
