ef8892aca1785d025f29aeafdf8affa6ae2aab85
kadoyau <el.psi.congroo@gmail.com>
2019-06-09 01:31:33 +0900

    API clientを使った結果を表示する

--- src/templates/Root.tsx
+++ src/templates/Root.tsx
@@ -1,19 +1,34 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SearchBar from '../organisms/SearchBar';
 import styles from './Root.module.css';
 import SearchList from '../organisms/SearchList';
+import ApiClient from '../utils/ApiClient';
 
 const RootTemplate = () => {
   const [query, setQuery] = useState('');
+  // YouTube APIの検索結果を格納するStateを定義
+  const [list, setList] = useState<gapi.client.youtube.SearchResult[]>([]);
   const handleChange = useCallback((q: string) => {
     setQuery(q);
   }, []);
 
+  // queryが更新されるたびにAPIを呼ぶ。useEffectはReactのrenderが終わったあとに呼ばれることが保証されている
+  useEffect(() => {
+    const searchList = async () => {
+      const { items } = await ApiClient.search(query);
+      setList(items!);
+    };
+
+    // queryに文字が入力されれば検索
+    if (query.trim() !== '') {
+      searchList();
+    }
+  }, [query]);
   return (
     <>
       <SearchBar onChange={handleChange} />
       <div className={styles.content}>
-        <SearchList query={query} list={[1, 2, 3, 4, 5]} />
+        <SearchList query={query} list={list} />
       </div>
     </>
   );
