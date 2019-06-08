de82a65ff04508aea63ae14987791d258ae44c45
kadoyau <el.psi.congroo@gmail.com>
2019-06-09 00:00:03 +0900

    yarn lint:js --fix

--- src/organisms/SearchBar.tsx
+++ src/organisms/SearchBar.tsx
@@ -1,41 +1,41 @@
-import {AppBar, InputBase, Toolbar, withStyles} from "@material-ui/core";
+import { AppBar, InputBase, Toolbar, withStyles } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import React, {useCallback} from "react";
+import React, { useCallback } from 'react';
 import styles from './SearchBar.module.css';
 
 interface Props {
-    onChange: (value: string) => void;
+  onChange: (value: string) => void;
 }
 
 const SearchBar = (props: Props) => {
-    const {onChange} = props;
-    // function componentで関数定義をする場合はuseCallbackを使うと、
-    // メモ化されるのでレンダリングのために再定義されるのを防げる
-    const handleChange = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            onChange(event.target.value);
-        },
-        []
-    );
-    return (
-        <AppBar position={"fixed"}>
-            <Toolbar variant="dense">
-                <div className={styles.search}>
-                    <SearchIcon fontSize="small"/>
-                    <StyledInputBase placeholder="search..." onChange={handleChange}/>
-                </div>
-            </Toolbar>
-        </AppBar>
-    );
+  const { onChange } = props;
+  // function componentで関数定義をする場合はuseCallbackを使うと、
+  // メモ化されるのでレンダリングのために再定義されるのを防げる
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      onChange(event.target.value);
+    },
+    []
+  );
+  return (
+    <AppBar position="fixed">
+      <Toolbar variant="dense">
+        <div className={styles.search}>
+          <SearchIcon fontSize="small" />
+          <StyledInputBase placeholder="search..." onChange={handleChange} />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 // withStyle()を使用してスタイルを上書きしている
 const StyledInputBase = withStyles({
-    root: {
-        flexGrow: 1,
-        marginLeft: '5px',
-        color: '#fff'
-    }
+  root: {
+    flexGrow: 1,
+    marginLeft: '5px',
+    color: '#fff'
+  }
 })(InputBase);
 
 export default SearchBar;
--- src/organisms/SearchList.tsx
+++ src/organisms/SearchList.tsx
@@ -1,25 +1,25 @@
-import {List, ListItem, ListSubheader} from "@material-ui/core";
+import { List, ListItem, ListSubheader } from '@material-ui/core';
 import React from 'react';
 
 interface Props {
-    query: string;
-    list: any[];
+  query: string;
+  list: any[];
 }
 
 const SearchList = (props: Props) => {
-    const {query, list} = props;
-    return (
-        <List
-            subheader={<ListSubheader>{`${query}の検索結果`}</ListSubheader>}
-            dense
-        >
-            {list.map(item => (
-                <ListItem key={item} button divider>
-                    {item}
-                </ListItem>
-            ))}
-        </List>
-    );
-}
+  const { query, list } = props;
+  return (
+    <List
+      subheader={<ListSubheader>{`${query}の検索結果`}</ListSubheader>}
+      dense
+    >
+      {list.map(item => (
+        <ListItem key={item} button divider>
+          {item}
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 export default SearchList;
