640240c9dbe7ae8dc410d3ad740275c90baaaac1
kadoyau <el.psi.congroo@gmail.com>
2019-06-24 02:00:06 +0900

    検索でdebounceする

--- src/templates/Root.tsx
+++ src/templates/Root.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import debounce from 'lodash.debounce';
 import SearchBar from '../organisms/SearchBar';
 import styles from './Root.module.css';
 import SearchList from '../organisms/SearchList';
@@ -8,25 +9,24 @@ const RootTemplate = () => {
   const [query, setQuery] = useState('');
   // YouTube APIの検索結果を格納するStateを定義
   const [list, setList] = useState<gapi.client.youtube.SearchResult[]>([]);
-  const handleChange = useCallback((q: string) => {
-    setQuery(q);
-  }, []);
 
-  // queryが更新されるたびにAPIを呼ぶ。useEffectはReactのrenderが終わったあとに呼ばれることが保証されている
-  useEffect(() => {
-    const searchList = async () => {
-      const { items } = await ApiClient.search(query);
-      setList(items!);
-    };
-
-    // queryに文字が入力されれば検索
-    if (query.trim() !== '') {
-      searchList();
-    }
-  }, [query]);
+  const searchList = useCallback(
+    debounce(async (q: string) => {
+      if (q.trim() !== '') {
+        const { items } = await ApiClient.search(q);
+        setQuery(q);
+        setList(items!);
+      } else {
+        console.log('から！！！');
+        // 入力テキストが空なら結果はない
+        setList([]);
+      }
+    }, 500), // 500ms待つ
+    []
+  );
   return (
     <>
-      <SearchBar onChange={handleChange} />
+      <SearchBar onChange={searchList} />
       <div className={styles.content}>
         <SearchList query={query} list={list} />
       </div>
